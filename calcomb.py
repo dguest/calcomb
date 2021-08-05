f51111400ab050cb56d62f3e18e1a9604f437c0a
Dan Guest <dguest@cern.ch>
2021-08-05 09:02:30 +0200

    Remove zoom passwords

--- calcomb.py
+++ calcomb.py
@@ -3,19 +3,23 @@
 """
 Make a more reasonable ics file from an unreasonable one
 """
+_raw_help = "Don't clean zoom links"
 
 from argparse import ArgumentParser
 from urllib.request import Request, urlopen
 from sys import stdout
 import re
+from textwrap import wrap
 
 def get_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('urls', nargs='+')
     parser.add_argument('-m','--matches', nargs='*', default=['HH-->4b'])
+    parser.add_argument('-r','--raw', action='store_true', help=_raw_help)
     return parser.parse_args()
 
 key_regex = re.compile('(SUMMARY|URL|DESCRIPTION):.+')
+zoom_password_regex = re.compile('([&?]pwd=)[A-Za-z0-9]+')
 
 class Event:
     keyed: dict
@@ -25,23 +29,42 @@ class Event:
         self.keyed = {}
         self.event = []
         self.active = False
+        self.last_key = None
 
+def wrap_lines(lines, clean):
+    wrapped = []
+    for line in lines:
+        if clean:
+            line = zoom_password_regex.sub(r'\1[REDACTED]', line)
+        wrapped += wrap(
+            line,
+            width=75,
+            subsequent_indent=" ",
+            drop_whitespace=False,
+            replace_whitespace=False,
+        )
+    return '\r\n'.join(wrapped + [''])
 
-def event_iter(file_like):
+def event_iter(file_like, clean=True):
     event = Event()
     for raw in file_like:
-        line = raw.decode('utf-8').rstrip()
+        line = raw.decode('utf-8').rstrip('\r\n')
         if line == 'BEGIN:VEVENT':
             if event.active:
                 raise Exception('found event in event')
             event.active = True
         if event.active:
+            if line.startswith(' '):
+                event.event[-1] += line[1:]
+                event.keyed[event.last_key] += line[1:]
+            else:
+                event.event += [line]
             if key_regex.match(line):
                 key, val = line.split(':',1)
                 event.keyed[key] = val
-            event.event += [line]
+                event.last_key = key
         if line == 'END:VEVENT':
-            yield event.keyed, '\r\n'.join(event.event + [''])
+            yield event.keyed, wrap_lines(event.event, clean=clean)
             event = Event()
 
 def run():
@@ -53,7 +76,7 @@ def run():
     )
     for url in args.urls:
         req = Request(url)
-        for edict, event in event_iter(urlopen(req)):
+        for edict, event in event_iter(urlopen(req), clean=(not args.raw)):
             if any(m in edict['SUMMARY'] for m in args.matches):
                 stdout.write(event)
     stdout.write(
